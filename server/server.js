ce1a8a54ec2156e915f58bc7bffc8b61b100087d
derangee <daksh_vashishtha@srmap.edu.in>
2025-01-29 16:42:54 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,19 @@
 const http = require('http');
 
 const server = http.createServer((req, res) =>{
-    console.log(req.url, req.method, req.headers);
+    res.setHeader("Content-Type", 'text/HTML');
+    res.write('<HTML>');
+    res.write('<body> <h1> Hey </h1> </body> ')
+    res.write('</HTML>')
+    res.end();
 })
 
-const PORT = 3001; 
+const PORT = 5000; 
 server.listen(PORT, ()=>{
     console.log(`Server running at PORT http://localhost:${PORT}`)
 });
+
+
 
 
 
