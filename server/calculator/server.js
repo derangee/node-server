01ffc5177488c04b46c6acc61d6628173193392f
derangee <daksh_vashishtha@srmap.edu.in>
2025-01-30 02:13:11 +0530

    Update server.js

--- server/calculator/server.js
+++ server/calculator/server.js
@@ -4,67 +4,74 @@ const { buffer } = require('stream/consumers');
  const handler = ((req,res)=>{
 
     if(req.url === '/') {
+        res.setHeader("Content-Type", 'text/HTML');
         res.write(
             `<HTML>
                 <body> 
-                    <h1> Calculator - using NodeJs! 
+                    <h1> Calculator - using NodeJs! </h1>
                     <br>
-                    <a href = "/calculator"> Go to Calculator </a>
+                    <br>
+                    <br>
+                    <br>
+
+                   <h2> <a href = "/calculator"> Go to Calculator </a> </h2>
                 </body> 
             </HTML>`
         )
         return res.end();
     }
 
-    else if(req.url === '/form' && req.method == "POST") {
-        const body  = [];
-        req.on('data', chunk =>{
-            console.log(chunk); //reading chunks  
-            body.push(chunk);
+    else if(req.url === '/calculator') {
+        res.setHeader("Content-Type", 'text/HTML');
+        res.write(
+            `<HTML>
+                <body> 
+                    <h1> Calculator </h1>
+                    <br>
+                    <br>
+
+                    <form action="/calculate-result" method="POST">
+                        <input type="text" name="firstnum"/> 
+                        <input type="text" name="secondnum"/> 
+                        <input type="submit"/>
+                    </form>    
+                </body> 
+            </HTML>`
+        )
+        return res.end();
+    }
+
+    else if(req.url === "/calculate-result" && req.method == "POST") {
+         
+        const obj = {} ;
+        const body = [];
+
+        req.on(('data') , chunks => {
+            body.push(chunks);
         })
 
-        req.on('end', ()=>{
-            const parsed = Buffer.concat(body).toString(); //changing the buffer data into string data
-            console.log(parsed);
+        let result = [];
 
-            const obj = {}; // empty object
-            const params = new URLSearchParams(parsed); //changing the string to readable lang 
+        req.on(('end') , () => {
+            const objdata = Buffer.concat(body).toString();
+            const params = new URLSearchParams(objdata);
+            const bodyobj = Object.fromEntries(params);
+            const result = Number(params.firstnum) + Number(params.secondnum)
+            
+            res.write(`
+                <html>
+                    <body>
+                        <h1> Result is ${result} </h1>
+                    </body>
+                </html>`        
+            )
 
-            for( const[key,value] of params.entries() ) {  //storing data in form of key value pair
-                obj[key] = value;
-            }
-
-            const bodyobj = Object.fromEntries(params); //another way of storing data in objects instead of for loop
-
-            console.log(obj)
-            //console.log(bodyobj); would also give same result
-            fs.writeFileSync('user.txt', JSON.stringify(obj)); //storing data in user.txt
-
-
+            return res.end();
         })
-
-
 
         res.statusCode = 302;
         res.setHeader('location', '/');
         return res.end();
-    }
-
-    else if(req.url === '/nav') {
-        res.write('<HTML>');
-        res.write('<body>');
-
-        res.write('<h3> <a href="/home"> Home </a> </h3> ')
-        res.write('<h3> <a href="/men"> Men </a> </h3> ')
-        res.write('<h3> <a href="/women"> Women </a> </h3> ')
-        res.write('<h3> <a href="/kids"> Kids </a> </h3> ')
-        res.write('<h3> <a href="/cart"> Cart </a> </h3> ')
-
-
-        res.write('</body>')
-        res.write('</HTML>');
-        return res.end();
-
     }
 
     else{
