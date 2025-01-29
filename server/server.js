bb18e60d58c1d114d424e34b92a4ed98832c3b51
derangee <daksh_vashishtha@srmap.edu.in>
2025-01-29 22:53:58 +0530

    sending res

--- server/server.js
+++ server/server.js
@@ -1,31 +1,29 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) =>{
 
     if(req.url === '/') {
-        res.setHeader("Content-Type", 'text/HTML');
         res.write('<HTML>');
         res.write('<body> <h1> Hey </h1> </body> ')
-        res.write('</HTML>')
-        return res.end();
-    }
-
-    else if(req.url === '/form && req.method == "POST"') {
-        res.setHeader("Content-Type", 'text/HTML');
-        res.write('<HTML>');
-        res.write('<body> <h1> Hey </h1> </body> ')
-        res.write('<form method="POST">')
+        res.write('<form method="POST" action ="/form">')
         res.write('<input type="text" name="username"/>')
         res.write('<br>');
+        res.write('<label for="male"> Male </label>')
         res.write('<input type="radio" name="gender" id="male" value="male"/>')
+        res.write('<label for="female"> Female </label>')
         res.write('<input type="radio" name="gender" id="female" value="female"/>')
         res.write('<br>');
         res.write('<input type="submit"/>')
         res.write('</form>')
         res.write('</HTML>')
+        return res.end();
+    }
+
+    else if(req.url === '/form' && req.method == "POST") {
+        fs.writeFileSync('user.text', 'daksh vashishtha');
         res.statusCode = 302;
         res.setHeader('location', '/');
-        return res.end();
     }
 
     else{
